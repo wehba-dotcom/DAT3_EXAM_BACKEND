143cb51edc445321c64161dc593efdee62dea12a
wehba <Lenovo@wehba.com>
2022-01-13 15:50:28 +0100

    backend last modify for  entity point

--- src/main/java/utils/SetupTestUsers.java
+++ src/main/java/utils/SetupTestUsers.java
@@ -11,15 +11,10 @@ public class SetupTestUsers {
 
   public static void main(String[] args) {
 
+
     EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
     EntityManager em = emf.createEntityManager();
     
-    // IMPORTAAAAAAAAAANT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    // This breaks one of the MOST fundamental security rules in that it ships with default users and passwords
-    // CHANGE the three passwords below, before you uncomment and execute the code below
-    // Also, either delete this file, when users are created or rename and add to .gitignore
-    // Whatever you do DO NOT COMMIT and PUSH with the real passwords
-
 
     User user = new User("user", "test1");
     User admin = new User("admin", "test2");
